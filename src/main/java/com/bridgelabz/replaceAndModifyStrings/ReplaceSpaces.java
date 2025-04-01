0105a411ca2bf0334db578e0ded24e0961afb4e0
Yagyata <yagyatasharmaa@gmail.com>
2025-04-01 22:58:59 +0530

    [Yagyata] ADD: Replace Multiple Spaces with a single space

--- src/main/java/com/bridgelabz/replaceAndModifyStrings/ReplaceSpaces.java
+++ src/main/java/com/bridgelabz/replaceAndModifyStrings/ReplaceSpaces.java
@@ -1,6 +1,6 @@
 package com.bridgelabz.replaceAndModifyStrings;
 
-public class MultipletoSingleSpace {
+public class ReplaceSpaces {
     public static void main(String[] args) {
         String input = "This  is  an example      with   multiple     spaces.";
         String replacedText = input.replaceAll("\\s+", " ");
