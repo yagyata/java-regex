b65e463b7524a2ef51090790fa75513d42887de3
Yagyata <yagyatasharmaa@gmail.com>
2025-04-01 15:41:06 +0530

    [Yagyata] ADD: java regex

---
+++ src/main/java/BasicRegexProblems/ValidateUsername.java
@@ -0,0 +1,19 @@
+package BasicRegexProblems;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ValidateUsername {
+    public static void main(String[] args) {
+        String regex = "^[a-zA-Z]+[a-zA-z0-9_]{5,15}";
+        Pattern pattern = Pattern.compile(regex);
+        String input = "yagyataA1_123";
+
+        Matcher matcher = pattern.matcher(input);
+        if(matcher.matches()) {
+            System.out.println("Valid Input");
+        } else {
+            System.out.println("Invalid Input");
+        }
+    }
+}
