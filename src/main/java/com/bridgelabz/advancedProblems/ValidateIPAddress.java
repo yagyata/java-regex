5c8d7977696a0a7cef9d94aa8933b08ff0ddba21
Yagyata <yagyatasharmaa@gmail.com>
2025-04-02 09:45:23 +0530

    [Yagyata] ADD:  Validate IP Address

--- src/main/java/com/bridgelabz/advancedProblems/ValidateIPAddress.java
+++ src/main/java/com/bridgelabz/advancedProblems/ValidateIPAddress.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 
 public class ValidateIPAddress {
     public static void main(String[] args) {
-        String regex = "^[0-2][0-9]{3}\\.[0-255]\\.[0-255]\\.[0-255]$";
+        String regex = "^(([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$"; //0-9 | 10-99 | 100-199 | 200-249 | 249-255
         Pattern pattern = Pattern.compile(regex);
         String ipAddress = "192.168.1.1";
         Matcher matcher = pattern.matcher(ipAddress);
